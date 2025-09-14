6f72a8a168d383787f0f8ce65999c5394c30d5db
shubhanshu-02 <shubhanshu004@gmail.com>
2025-09-15 01:05:06 +0530

    add prod server as default
    Signed-off-by: shubhanshu-02 <shubhanshu004@gmail.com>

--- evently_booking_platform/main.py
+++ evently_booking_platform/main.py
@@ -143,6 +143,10 @@ app = FastAPI(
     },
     servers=[
         {
+            "url": "https://evently-booking-platform-latest.onrender.com",
+            "description": "Production server"
+        },
+        {
             "url": "http://localhost:3000",
             "description": "Development server (HTTP)"
         },
